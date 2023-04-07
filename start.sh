81bcc96761c4f9e0e5f2e233311a253add9156cb
Amourxd <126812456+Amourxd@users.noreply.github.com>
2023-04-07 22:17:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Amourxd/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/Amourxd/VCPlayerBot /main
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Amourxd/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/Amourxd/VCPlayerBot -b $BRANCH /main
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
